ba273a3cf658928f0bd46f59b02f0b0bd0221717
witt007 <witt007@qq.com>
2023-04-01 00:14:30 +0800

    refactoring code is done

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cluster from "cluster";
 import https from "https";
+import http from "http";
 import fs from 'fs'
 //@ts-ignore
 import websocket from "ws";
@@ -11,8 +12,8 @@ import cookieParser from "cookie-parser";
 
 import path from "path";
 import routerChatbot from "./router/chatbot";
+import { init as initWs } from './API/sockets'
 let socket: websocket.WebSocket;
-
 
 const app = express();
 
@@ -23,59 +24,34 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 //app.set("views", path.join(__dirname, "views"));
 app.set("view engine", 'ejs');
+
 app.use(function doCors(req, res, next) {
   console.log("middleware-", req.url);
-
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin,Content-Type,Accept,X-Request-With');
-  if (req.method == 'OPTION') {
-    res.setHeader("Access-Control-Allow-Methods", 'GET,POST,PUT,PATCH,DELETE');
-  }
-  //res.clearCookie('chatbotToken')
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  //res.clearCookie('chatbotToken');
+  
   next();
+ 
+  
 });
-app.get('/',(req,res)=>{
-  verifyLoggedIn(req, res);
+app.get('/', (req, res) => {
+  //verifyLoggedIn(req, res);
 
 })
-app.use('/', routerChatbot);
+app.use('/chat', routerChatbot);
 const cert = fs.readFileSync(path.join(__dirname, 'certs/v2ray.pem'));
 const certKey = fs.readFileSync(path.join(__dirname, 'certs/v2ray.key'));
 const server = https.createServer({ cert, key: certKey }, app);
+//const server = http.createServer( app);
 server.listen(82);
 
+initWs(server);
 
 
 
-const wsServer = new websocket.WebSocketServer({ server });
-
-wsServer.on(
-  "connection",
-  async function (sk: websocket.WebSocket, req: IncomingMessage) {
-    console.log("connected" + Math.random(), sk.url);
-    socket = sk;
-
-    socket.onmessage = function (ev: websocket.MessageEvent) {
-      console.log("Witt:received data", ev.data);
-      const data = ev.data;
-      if (data === "qrcode") {
-        /*    if (bot.isLoggedIn)
-             return socket.send(JSON.stringify({ type: "hasloggedIn" }));
-           */
-      }
-    };
-
-    socket.onclose = function (ev: websocket.CloseEvent) {
-      console.log("Witt:closed", ev.reason);
-    };
-    socket.onerror = function (ev: websocket.ErrorEvent) {
-      console.log("Witt:error", ev.message);
-    };
-    socket.onopen = function (ev: websocket.Event) {
-      console.log("Witt:opened", ev.target);
-    };
-  }
-);
 
 /**
  * 
@@ -86,8 +62,18 @@ wsServer.on(
 function verifyLoggedIn(req: Request, res: Response) {
   let token = req.cookies["chatbotToken"];
   if (!token) {
-    res.redirect('/login');
+    res.redirect('/chat/login');
   }
 }
+
+/* getUsers<userData>().then((data) => {
+  const token = Object.values(data).find((user) => {
+    return !user.isLoggedOut
+  })?.token
+
+  
+}) */
+console.log('程序已启动！');
+
 
 
--- router/chatbot/index.ts
+++ router/chatbot/index.ts
@@ -1,465 +1,104 @@
 import express, { Response, Request } from "express";
-import { ContactSelf, ScanStatus, WechatyBuilder } from "wechaty";
-import { PuppetPadlocal } from 'wechaty-puppet-padlocal'
-import { MessageInterface, WechatyInterface } from "wechaty/impls";
-import https from "https";
-import { IncomingMessage } from "http";
-import fs from "fs";
-import path from "path";
-import { Configuration, OpenAIApi, CreateCompletionRequest, CreateCompletionResponse } from "openai";
-import { Blob } from "buffer";
-import { RedisClientType, createClient } from "redis";
-import { WechatyEventListenerMessage, WechatyEventListenerScan, WechatyEventListenerLogin, WechatyEventListenerLogout, WechatyEventListenerError } from "wechaty/dist/esm/src/schemas/wechaty-events";
+import { WechatyInterface } from "wechaty/impls";
+import evEmitter from "../../API/events";
+
+import { createAndRunBot, getUser, bindBotEvt, getMapBots, getResponseData } from "../../API/wechaty";
+import { fork } from "child_process";
+import path from 'path'
+import { tb } from "../../data";
+import stream from 'stream'
+
 const router = express.Router({ caseSensitive: true });
-import axios, { AxiosResponse, AxiosRequestHeaders, AxiosHeaders } from "axios";
-//const bot = initBot();
-const openai = initOpenAI();
 
-interface tableData {
+//FIXME:Each user can only possess one token. here is not best way to set a token. Seems that the function is just a way to set a cookie
+function getBotTokenOrset(req: Request, res: Response): string {
+  let token = req.cookies['chatbotToken'];
 
-}
-type username = string
-type user = { name?: string, token: string, isLoggedOut?: boolean, contact?: any, phone?: any }
-
-interface userData extends tableData {
-  [key: username]: user
-}
-type allBots = Map<username, WechatyInterface>;
-type mapUsrRequestInfo = WeakMap<WechatyInterface, { token: username, res?: Response }>
-interface log extends tableData {
-  [key: username]: string
-}
-type botListenners = {
-  onmessage: WechatyEventListenerMessage,
-  onscan: WechatyEventListenerScan,
-  onlogin: WechatyEventListenerLogin,
-  onlogout: WechatyEventListenerLogout,
-  onerror: WechatyEventListenerError
-}
-class Database {
-  private tables: Map<string, Table> = new Map();
-  databaseName: string
-  redis: RedisClientType
-  constructor(databaseName: string) {
-    this.databaseName = databaseName;
-    this.redis = createClient({
-      password: 'OyHV5ACyC3x7tKrXYDqWkeMYWxaTpMsu',
-      socket: {
-        host: 'redis-19723.c302.asia-northeast1-1.gce.cloud.redislabs.com',
-        port: 19723//,username:"default"
-      }
-    });
-    this.redis.connect();
-
-    this.redis.on('connect', () => console.log('Redis Client Connected'));
-  }
-  createTable(tablename: string): Table {
-    return new Table(tablename, this.redis);
-  }
-
-  deleteTable(tablename: string) {
-    const table = this.tables.get(tablename);
-    if (table) {
-      try {
-        // table.delete()
-      } catch (error) {
-        console.log('failed to delete table', error);
-
-      }
-
-    }
-  }
-}
-
-
-class Table {
-  tablename: string
-  redis: RedisClientType
-  constructor(tablename: string, redis: RedisClientType) {
-    this.tablename = tablename;
-    this.redis = redis;
-  }
-
-  private dataTemplate = JSON.stringify({ users: {}, msgRecord: {} })
-
-  private readFile<T extends tableData>(): Promise<T> {
-    const self = this;
-    return new Promise<T>(async (resolve) => {
-      const v = await this.redis.get(this.tablename);
-      resolve(JSON.parse(v || '{}'));
-    })
-
-    new Promise<T>((resolve) => {
-      const userpath = path.join(process.cwd(), 'data/', self.tablename + '.json');
-      fs.access(userpath, fs.constants.F_OK, (error: any) => {
-
-        if (error) {
-
-          fs.writeFile(userpath, '{}', {}, (err) => {
-            console.log(this.tablename, 'failed to read');
-            readFile()
-          })
-        } else readFile()
-        function readFile() {
-
-          fs.readFile(userpath, function (error, data) {
-            const datastr = data.toString(); console.log(typeof datastr);
-
-            if (datastr) {
-              resolve(JSON.parse(datastr));
-
-            } else console.log('failed to read the json file'), resolve(JSON.parse(self.dataTemplate))
-          })
-        }
-
-
-      })
-    })
-
-  }
-  delete() {
-
-  }
-  writeData<T extends tableData>(data: T) {
-    const dataStr = JSON.stringify(data);
-    console.log('write data', data);
-
-    return this.redis.set(this.tablename, dataStr);
-
-    const userpath = path.join(process.cwd(), 'data/', this.tablename + '.json')
-    fs.writeFile(userpath, dataStr, {}, function (error) {
-      error && console.log('failed to write user data', error) || (2)
-
-    })
-
-
-
-  }
-  readData<T extends tableData>(): Promise<T> {
-    return this.readFile<T>();
-  }
-
-
-}
-
-function getUsers<T extends tableData>(tb: Table): Promise<T> {
-  return tb.readData<T>();
-}
-function setUsers(data: userData) {
-  return tb.writeData(data);
-}
-async function deactiveUser(token: username) {
-  getUsers<userData>(tb).then((data) => {
-    data[token].isLoggedOut = true;
-    setUsers(data);
-  })
-}
-
-
-function getOrSetBotToken(req: Request, res: Response): string {
-  let token = req.cookies["chatbotToken"];
   if (!token) {
-    token = "Witt:" + Math.random().toString();
-    res.cookie("chatbotToken", token); //.setHeader("Set-Cookie", ["chatbotToken=" + token]);
+    token = Math.random() * Math.pow(10, 16) //process.env.PUPPET_TOKENS;
+
+    res.cookie("chatbotToken", token, { httpOnly: true }); //.setHeader("Set-Cookie", ["chatbotToken=" + token]);
 
     console.log("cookie is set", req.cookies["chatbotToken"]);
-  } else {
-
   }
   console.log("client token", token);
   return token;
 }
 
-function getInitBotListenners(): botListenners {
-  return {
-    onerror(this: WechatyInterface, err) {
-      this.reset().then(() => {
-        this.logout();
-        console.log('encoutered a problem so it reset');
+const mapBots = getMapBots()
 
-      })
-    },
-    async onlogin(this: WechatyInterface, self: ContactSelf) {
-      console.log("success login");//todo 只要有请求就被迫下线
-      const info = weakMapUsrRequestInfo.get(this);
-      let contactArr = await this.Contact.findAll();
-      const phone = await self.phone()
-      const contact = await contactArr.map(async (contact) => {
-        const city = contact.city()
-        const name = contact.name();
-        const gender = contact.gender()
-        const phone = await contact.phone();
-        const province = contact.province();
-        return { city, name, gender, phone, province }
-      })
-      if (info?.token) {
-        getUsers<userData>(tb).then(async (data) => {
-          const alias = await self.alias()
-          console.log('has ..........................', alias, data);
+function communicateWithBot(res: Response) {
+  evEmitter.on('responseQR', function (buffData: Buffer) {
+    console.log('responseQR again');
 
-          data[info.token] = { isLoggedOut: false, token: info.token, name: alias, contact, phone }
-          setUsers(data);
-
-        })
-      } else console.log('witt:encoutered error Onlogin');
-
-    },
-    onlogout(this: WechatyInterface) {
-      console.log("logout");
-      const token = weakMapUsrRequestInfo.get(this)?.token
-      token &&
-        (deactiveUser(token)/* , mapBots.delete(token) */) || console.log('witt:encoutered error Onlogout');
-
-    },
-    onscan(this: WechatyInterface, qrcode: string, status) {
-      console.log("onscan");
-      let res = weakMapUsrRequestInfo.get(this)?.res
-
-      if (status == ScanStatus.Waiting || status == ScanStatus.Timeout) {
-        const qrcodeImageUrl: string = [
-          "https://api.qrserver.com/v1/create-qr-code/?data=",
-          encodeURIComponent(qrcode),
-        ].join("");
-        console.log("Scanning...", qrcodeImageUrl);
-        https
-          .get(qrcodeImageUrl, function (req: IncomingMessage) {
-            req.on("data", (chunk: Buffer) => {
-              console.log('chunk', chunk);
-
-              res?.status(200).type("jpeg").end(chunk);
-            });
-          })
-          .on("error", (error) => {
-            console.log("https.get", error);
-          });
-      }
-    },
-    onmessage(this: WechatyInterface, msg: MessageInterface) {
-      if (msg.type() !== this.Message.Type.Text) return
-      if (!msg.text()) {
-        //return;
-      }
-      console.log("received a message", msg
-        .talker()
-        .name());
-
-
-      responseMsg(this, msg);
-    }
-  }
-}
-
-let currentBot:WechatyInterface
-router.get("/login", function (req, res) {
-  const token = req.cookies['chatbotToken']
-  const bot = mapBots.get(token);
-  if (token===currentBot.name()) { //the same user
-    
-  }else{
-    
-    currentBot.Location()
-  }
-  //todo find actived bot
-
-  if (bot && bot.isLoggedIn)
-    res.end("You have logged in!");
-  else
-    //bot && mapBots.delete(token), 
-    mapBots.clear(),
-      createAndRunBot(getOrSetBotToken(req, res)).then((bot) => {
-
-        bindBotEvt.call(bot, getInitBotListenners())
-        mapBots.set(token, bot);
-        weakMapUsrRequestInfo.set(bot, { token, res }); console.log('create and run bot successfully');
-
-      })
-  //what time?
-});
-
-
-
-function outputLog(token: username = "alternative", obj: {}) {
-  /*   const args: any[] = Object.entries(obj).map((v, i) => {
-      return `Witt: the property ${v[0]} value is ${v[1]}\n`;
-    }); */
-
-  tbLogs.readData<log>().then((data: log) => {
-    data[token || 'alternative'] += 'newer:' + JSON.stringify(obj);
-    tbLogs.writeData<log>(data);
-    console.log('logs', obj);
+    res.status(200).type("jpeg").end(buffData);
   })
 }
 
-
-
-function bindBotEvt(this: WechatyInterface, listeners: botListenners) {
-  this.once("scan", listeners.onscan);
-  this.once("stop", () => {
-    console.log("Bot stopped");
-  });
-
-  this.once("login", listeners.onlogin);
-  this.once("logout", listeners.onlogout);
-  this.on("message", listeners.onmessage);
-  this.on("error", listeners.onerror)
-}
-async function createAndRunBot(
-  token: string
-): Promise<WechatyInterface> {
-
-  const bot = WechatyBuilder.build({
-    //@ts-ignore
-    puppet: new PuppetPadlocal({ token: "67f5cc1db0f84923827097fd1bfe6e7d", /* timeoutSeconds: 8000  */ }),
-    name: "wechat",
-    /*  puppetOptions: {
-       uos: true,
-       //token,
-     }, */
-  });
-  console.log("entered");
-
-
-  await bot.start().then(() => {
-    console.log("chatbot started");
-    return bot;
-  }).catch((error) => {
-    console.log("Witt:started error", error);
-    bot.start();
-  });
-
-  return bot;
-}
-
-function initOpenAI(): OpenAIApi {
-  const config: Configuration = new Configuration({
-    organization: "Witt",
-    apiKey: "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",
-  });
-  return new OpenAIApi(config);
-}
-
-async function responseMsg(bot: WechatyInterface, msg: MessageInterface) {
-  let text = msg.text();
-  let talker = await msg.talker();
-  let alias = await talker.name() as string
-  let phones = await talker.phone()
-  let isSentByMe = msg.self();
-  const myself = bot.currentUser.name()//msg.listener()?.name()
-  /*   msg.toSayable().then((say) => {//相当于拷贝人家的文字从而转发
-      console.log('sayable', say);
-    }) */
-  const MentionedMe = await msg.mentionSelf();
-  const room = await msg.room();
-  const date = msg.date();
-  console.log('isSentByMe', isSentByMe);
-
-  if (!text) {
-    return
-  }
-  if (/^( )/.test(text) || !isSentByMe) {
-    const headers: AxiosRequestHeaders = new AxiosHeaders();
-    headers.setAuthorization('Bearer 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');
-    headers.setContentType('application/json');
-    //@ts-ignore
-    const data: CreateCompletionRequest = {
-      max_tokens: 2000,
-      prompt: text, temperature: 0.5,
-      //top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-      // stop: ["Witt","YOU"],
+router.post("/login", async function (req, res) {
+  const token = getBotTokenOrset(req, res);
+  let bot = mapBots.get(token);
+  console.log('token and bot in /login', token, bot?.name());
+  if (!bot)
+    createAndRunBot(token).then((result) => {
+      //communicateWithBot(res)
+      res.end("starting successfully");
+    }).catch(() => {
+      res.end(null);
+    })
+  else { //the same user
+    // return bot.reset().then(()=>{});
+    if (!token) return res.end("seems to encountered a problem: there does not exist a token!")
+    const user = await getUser(token);//getting data from redis must has registered user
+    const dataFromOnscan:Buffer = getResponseData();
+    if (dataFromOnscan) {
+      //if exists data, then it demonstrate logged out but the bot still not dispose;
+      //if (!user.isLoggedIn)
+       bot.reset().then(() => {console.log('reset...');
+       res.end("starting successfully"); });
+      /*  {
+        const pass=new stream.PassThrough();
+        pass.end(dataFromOnscan);
+        pass.pipe(res);
+       } */
+    } else {
+      res.end('You have logged in!')
     }
-    const Lm = ["gpt-4", 'text-davinci-003','gpt-3.5-turbo']
-    const res: CreateCompletionResponse = await axios.post(`https://api.openai.com/v1/engines/${Lm[2]}/completions`,
-      data, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer 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`
-      }
-    }).then((res: AxiosResponse) => {
-      console.log('status', res.status);
-
-      if (res.status == 200) {
-        return res.data;
-      }
-
-    });
-    /*   openai.createCompletion({
-        prompt: text ||
-          "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
-        model: "text-davinci-003",
-        //prompt: "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-        temperature: 0,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["\n"],
-      }, { headers }).then((res) => {
-        let say = res.data.choices[0].text || "";
-        console.log('it said', res.data.choices);
-    
-      }) */
-
-
-    //msg.say(say);
-
-    console.log('it said', res.choices);
-    const resText = res.choices[0].text?.trim();
-    resText && msg.say(resText);
-    outputLog(weakMapUsrRequestInfo.get(bot)?.token, { "消息日期：": date, '消息：': alias + ' ' + phones + '说：' + text, resText: myself + '回复道：' + resText, "是否@了我：": MentionedMe, '群聊：': room?.memberAll?.() });
-
-
   }
+  /*   if (!bot) //creating
+    else {
+      const user = await getUser(token);//getting data from redis must has registered user
+      console.log('user', user);
+      if (!user?.isLoggedIn) { //the same user
+        //  resData && res.end(resData);// Passively send msg
+        responseData(res);
+  
+   
+        //res.end("You have logged in!")
+  
+      } else {
+        res.end('You have logged in!')
+      }
+    } */
 
-}
+});
+
+router.get('/', async function (req: Request, res: Response) {
+  return
+  /*   const token = req.cookies['chatbotToken'];
+    const bot = mapBots.get(token);
+    console.log('chat/ entered', token, bot);
+  
+    if (!bot) {
+      createAndRunBot(getOrSetBotToken(req, res)).then((bot) => {
+        res.redirect('/chat/login?' + Math.random() * Math.pow(10, 16))
+      })
+    } else res.end('the bot already started') */
+})
+
 
 
 export default router;
 
-//if logged out then pop it out from users;
-
-//let currentBot:WechatyInterface;
-
-
-
-
-//only init Bots with logged in
-async function InitBots(map: allBots, mapBotToken: mapUsrRequestInfo) {
-
-  const userTB: userData = await getUsers<userData>(tb);
-  userTB && Object.values(userTB).map((user: user) => {
-    console.log('initbot', user);
-
-    user.isLoggedOut || createAndRunBot(user.token).then((bot: WechatyInterface) => {
-      bindBotEvt.call(bot, getInitBotListenners())
-      map.set(user.token, bot);
-      mapBotToken.set(bot, { token: user.token });
-    })
-
-
-  })
-}
-
-const database = new Database('chatbot');
-const tb = database.createTable('users');
-const tbLogs = database.createTable('logs');
-const mapBots: allBots = new Map()
-const weakMapUsrRequestInfo: mapUsrRequestInfo = new WeakMap<WechatyInterface, { token: username, res?: Response }>();
-
-/* tb.writeData({"witt":0}).then(()=>{
-  tb.readData().then((data)=>{
-console.log(data);
-  })
-}) */
-//InitBots(mapBots, weakMapUsrRequestInfo)
-
-/* process.on("beforeExit", function () {
-  mapBots.forEach((bot) => {
-    bot.logout();
-    const token = weakMapUsrRequestInfo.get(bot)?.token;
-    token && deactiveUser(token);
-  })
-}) */
 
 
